21951ea96d53dbe2a90c2e7b0bf2a337b184eee6
ZetecR-cloud <nabahiroki@gmail.com>
2026-01-29 20:57:22 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "ARI Safety Intelligence",
+  description: "ARI Safety Intelligence",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
